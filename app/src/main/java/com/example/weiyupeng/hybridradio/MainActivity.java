d1bc9d3b2d9787fd49d06ad81721bd4658d58ebe
weiyupeng <619728251@qq.com>
2018-06-07 16:40:41 -0400

    add Navi

--- app/src/main/java/com/example/weiyupeng/hybridradio/MainActivity.java
+++ app/src/main/java/com/example/weiyupeng/hybridradio/MainActivity.java
@@ -1,5 +1,6 @@
 package com.example.weiyupeng.hybridradio;
 
+import android.content.Intent;
 import android.content.res.Configuration;
 import android.content.res.Resources;
 import android.support.constraint.ConstraintLayout;
@@ -7,6 +8,7 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.CardView;
 import android.util.Log;
+import android.view.View;
 import android.widget.ImageView;
 import android.widget.TextView;
 
@@ -18,7 +20,17 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         matchScreenSize();
+        View view = findViewById(R.id.main);
+        view.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Log.d("Hello","hello");
+                Intent intent = new Intent(view.getContext(),HomePageActivity.class);
+                startActivity(intent);
+            }
+        });
     }
+
 
     // set the size before render screen
     void matchScreenSize(){
